07a7111797db7ac0fca02cacec2de341ea5f95f7
Michael Giebner <mikegiebner@gmail.com>
2019-12-01 12:24:14 -0500

    Refactored using redux

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import React, { Component } from "react";
 import user from "./images/user.png";
 import "./App.css";
 
+import reducer from "./reducers"
+import { createStore } from "redux"
+
+const initialState = {
+  name: "Alex Bakery",
+  description: "Software Engineer, Speaker, and Occasional Model",
+  likes: "Cats, Wine, and Black dresses",
+  location: "localhost"
+}
+
+const store = createStore(reducer, initialState)
+
 class App extends Component {
-  state = {
-    name: "Alex Bakery",
-    description: "Software Engineer, Speaker, and Occasional Model",
-    likes: "Cats, Wine, and Black dresses",
-    location: "localhost"
-  };
+
   render() {
-    const { name, description, likes, location } = this.state;
+    const { name, description, likes, location } = store.getState();
     return (
       <div className="App">
         <section className="User__img">
